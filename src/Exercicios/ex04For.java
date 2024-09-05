bf99d90b3b026debe25a0dbb0d559931bc3a8714
Pedro Cardoso <pedrocoscar@gmail.com>
2024-09-05 16:18:53 -0300

    Exercicio For

---
+++ src/Exercicios/ex04For.java
@@ -0,0 +1,12 @@
+package Exercicios;
+
+public class ex04For {
+    public static void main(String[] args) {
+        // Imprima todos os números pares de 0 até 1000000
+        for(int i = 0; i <= 1000000; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
