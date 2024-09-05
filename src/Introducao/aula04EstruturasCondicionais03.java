04e6f8d9dc77ff60d916e2d1b5bd6adc58639ffa
Pedro Cardoso <pedrocoscar@gmail.com>
2024-09-05 14:37:08 -0300

    Estruturas Condicionais

---
+++ src/Introducao/aula04EstruturasCondicionais03.java
@@ -0,0 +1,11 @@
+package Introducao;
+
+public class aula04EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        // Doar se salario > 2000
+        double salario = 2500;
+
+        String resultado = (salario > 2000) ? "Vou doar 500" : "Não vou doar";
+        System.out.println(resultado);
+    }
+}
