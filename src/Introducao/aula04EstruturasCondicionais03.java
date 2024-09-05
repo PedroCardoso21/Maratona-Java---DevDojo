e63dd47be12f5774db9022a2c5fdf645946851c2
Pedro Cardoso <pedrocoscar@gmail.com>
2024-09-05 15:02:08 -0300

    Fim das Estruturas Condicionais

--- src/Introducao/aula04EstruturasCondicionais03.java
+++ src/Introducao/aula04EstruturasCondicionais03.java
@@ -4,7 +4,7 @@ public class aula04EstruturasCondicionais03 {
     public static void main(String[] args) {
         // Doar se salario > 2000
         double salario = 2500;
-
+        //Ternario
         String resultado = (salario > 2000) ? "Vou doar 500" : "Não vou doar";
         System.out.println(resultado);
     }
