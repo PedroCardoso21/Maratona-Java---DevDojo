f53399a537d37f7b96e7866431b608c83ec28553
Pedro Cardoso <pedrocoscar@gmail.com>
2024-09-11 15:51:05 -0300

    Private, Get e Set

---
+++ src/javacore/Bintroducaometodos/teste/PessoaTest01.java
@@ -0,0 +1,13 @@
+package javacore.Bintroducaometodos.teste;
+
+import javacore.Bintroducaometodos.dominio.Pessoa;
+
+public class PessoaTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Naruto");
+        pessoa.setIdade(18);
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+    }
+}
